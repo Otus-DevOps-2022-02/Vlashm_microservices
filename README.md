0c4c37e5bf406b9a9ab070be5d7404b812495c22
Vladislav Ashmarin <vlad_83@mail.ru>
2022-05-17 19:52:26 +0300

    Home Work #12

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Vlashm_microservices
 Vlashm microservices repository
+
+## Домашнее задание 12
+
+- Установлен Docker
+- С помощью Docker machine создан Docker хост в YC
+- Создан образ с приложением и базой данных.
+- Из созданного образа запущен контейнер.
+- Образ загружен на Docke Hub
+
+### Задание со *
+
+В директории *docker-monolith/infra* созданы прототипы:
+- Ansible:
+    - Плэйбук *install_docker.yml* для установки Docker
+    - Плэйбук *install_app.yml* для запуска созданного образа
+    - Подключен динамический инвентарь *yc_compute*
+- Packer:
+    - Создает *image* с установленным Docker
+- Terraform:
+    - Поднимает инстансы в заданном количестве
---
+++ docker-monolith/infra/terraform/main.tf
@@ -0,0 +1,32 @@
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+resource "yandex_compute_instance" "reddit_docker" {
+  name  = "reddit-docker-${count.index}"
+  zone  = var.zone_compute_instance
+  count = var.count_instance
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
